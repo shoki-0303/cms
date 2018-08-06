d7f620691dc4d9167d5a446ae73a2cf73d9bed43
shoki-0303 <shoki522.33333@gmail.com>
2018-08-06 14:42:57 +0900

    記事の詳細画面（仮置き）

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
+  resources :articles, only: [:show]
   resources :users, only: [:edit, :update]
   resources :drafts, only: [:index, :new, :create, :edit, :update]
 end
