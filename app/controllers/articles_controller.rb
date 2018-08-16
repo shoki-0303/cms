d2643d6e0cc2e02656309bf091b7008dacaf00c6
shoki-0303 <shoki522.33333@gmail.com>
2018-08-16 14:40:32 +0900

    定数名を変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
   before_action :set_ranking, only: [:index, :show]
 
   def index
-    @articles = Article.page(params[:page]).per(Articles_Per_Page).where(release: true).order("created_at DESC")
+    @articles = Article.page(params[:page]).per(ARTICLES_PER_PAGE_NUMBER).where(release: true).order("created_at DESC")
   end
 
   def show
