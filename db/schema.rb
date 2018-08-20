3d84eb20efc63e7f58b4e54a7018bebba891892b
shoki-0303 <shoki522.33333@gmail.com>
2018-08-20 16:54:01 +0900

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,23 @@ ActiveRecord::Schema.define(version: 20180814084525) do
     t.string "slug_name"
   end
 
+  create_table "girls", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "nickname", null: false
+    t.string "email", default: "", null: false
+    t.text "image", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
     t.index ["name"], name: "index_tags_on_name"
